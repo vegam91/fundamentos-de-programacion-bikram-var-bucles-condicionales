20b3585f86d9c6675b1e161df21a2963ca9e0847
vegam91 <vegamartinoj@gmail.com>
2023-05-29 17:32:02 +0200

    final

---
+++ index.js
@@ -0,0 +1,77 @@
+let variableSinValor = undefined;
+let booleano1 = true;
+let booleano2 = false;
+const PI = 3.14;
+const TAU = 2* PI;
+let booleanoAnd = booleano1 == booleano2;
+let booleanoNot = !booleano1;
+let booleanoMix0 = (booleano1 || booleano2) && (booleano1 || (!booleano1 &&  !booleano2));
+// Operadores
+let incrementarDesp = 2;
+let resultadoDesp = incrementarDesp++;
+let incrementarAntes = 2;
+let resultadoAntes = ++incrementarAntes;
+
+let contarHasta10_2 = 0
+
+for (let i= 0; i <= 10; i ++) [
+    contarHasta10_2 = 10, 
+    (contarHasta10_2 === 10)
+]
+
+let postI = 0
+let postJ = 0
+for ( let i= 0; i < 11; i ++)[
+    postI += postJ++, 
+]
+
+let sumaPares = 0;
+
+for (let i = 0; i<10; i++)
+{
+
+     if (i % 2 == 0  )
+     sumaPares += i;
+}
+   
+13
+let variableValorNumerico = 12
+
+14//
+const MiNombre = "Vega"
+//15//
+const MiNumeroFav = 8
+//16//
+const booleanoOr = booleano1 || booleano2  
+//17//
+let booleanoMix1 = (booleano1 || variableValorNumerico >= MiNumeroFav);
+//18//
+let seisNoEsNueve = 6 !== 9
+//19
+let booleanoMix2 = (variableValorNumerico > 0) ||  -(MiNumeroFav * TAU);
+//20
+let valorSuma = MiNumeroFav + variableValorNumerico
+//21
+let valorResta = MiNumeroFav - variableValorNumerico
+//22
+let valorMultiplicacion = MiNumeroFav * variableValorNumerico
+//23
+let valorDivision = MiNumeroFav / 3
+//24
+let contarHasta10 = 0
+for (let i= 0; i <= 10; i ++) [
+    contarHasta10 = 10, 
+    (contarHasta10 === 10)]
+//25
+let preI = 0
+let preJ = 0
+for ( let i= 0; i < 11; i ++)[
+    preI += ++preJ, 
+]
+//26
+let sumaImpares= 0 
+for (let i = 0; i<10; i++)
+{
+    if (i % 2 !== 0  )
+     sumaImpares += i;
+}
